89400dc68976e3c440ca966fb2bc1538603e79f4
Jupiter <619990679@qq.com>
2015-12-04 14:55:33 +0800

    loading页修改
    loading页修改

--- app/src/main/java/com/tian/zhihu/ui/activity/SplashActivity.java
+++ app/src/main/java/com/tian/zhihu/ui/activity/SplashActivity.java
@@ -1,18 +1,15 @@
 package com.tian.zhihu.ui.activity;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
-import android.widget.ImageView;
 
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 import com.tian.zhihu.R;
 import com.tian.zhihu.base.BaseActivity;
+import com.tian.zhihu.base.BitmapCache;
 import com.tian.zhihu.constant.AppConstant;
 import com.tian.zhihu.network.NetworkHelper;
 import com.tian.zhihu.network.UIDataListener;
@@ -20,27 +17,27 @@ import com.tian.zhihu.network.VolleyQueueController;
 import com.tian.zhihu.network.api.GetStartImageHelper;
 import com.tian.zhihu.network.bean.StartImage;
 import com.tian.zhihu.utils.LogUtils;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import com.tian.zhihu.utils.ValueUtils;
 
 /**
  * Created by tianshuguang on 15/11/30.
  */
-public class SplashActivity extends BaseActivity implements UIDataListener<StartImage> {
+public class SplashActivity extends BaseActivity implements UIDataListener<StartImage>,Animation.AnimationListener {
 
-    private ImageView loading_start;
+    private NetworkImageView img_start;
+
     private NetworkHelper<StartImage> imageHelper;
+
+    private RequestQueue mQueue;
 
     @Override
     protected void installViews() {
-        setContentView(R.layout.acty_loading);
+        setContentView(R.layout.acty_splash);
         setTooBarVisible(View.GONE);
 
-        loading_start=(ImageView)this.findViewById(R.id.loading_start);
+        img_start=(NetworkImageView)this.findViewById(R.id.img_start);
 
-        LogUtils.e(TAG, "acty_loading");
+        LogUtils.e(TAG, "acty_splash");
         imageHelper=new GetStartImageHelper(this);
         imageHelper.setUiDataListener(this);
         imageHelper.sendPostRequest(AppConstant.BaseUrl + AppConstant.method_startimage, null);
@@ -55,81 +52,48 @@ public class SplashActivity extends BaseActivity implements UIDataListener<Start
     public void onDataChanged(StartImage data) {
         LogUtils.d(TAG,"text=="+data.text);
         LogUtils.d(TAG, "img==" + data.img);
+        //使用volly框架直接加载图片
         initImage(data.img);
     }
 
     @Override
     public void onErrorHappened(String errorCode, String errorMessage) {
         showToast("errorCode==" + errorCode + "errorMessage==" + errorMessage);
-        startActivity();
-    }
-
-    private void initImage(final String img) {
-        File dir = getFilesDir();
-        final File imgFile = new File(dir, "start.jpg");
-        if (imgFile.exists()) {
-            loading_start.setImageBitmap(BitmapFactory.decodeFile(imgFile.getAbsolutePath()));
-        } else {
-            loading_start.setImageResource(R.mipmap.start);
-        }
-
-        final ScaleAnimation scaleAnim = new ScaleAnimation(1.0f, 1.2f, 1.0f, 1.2f,
-                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
-                0.5f);
-        scaleAnim.setFillAfter(true);
-        scaleAnim.setDuration(3000);
-        scaleAnim.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                ImageRequest irequest = new ImageRequest(img,
-                        new Response.Listener<Bitmap>() {
-                            @Override
-                            public void onResponse(Bitmap bitmap) {
-                                saveBitmap(imgFile, bitmap);
-                                startActivity();
-                            }
-                        }, 0, 0, Bitmap.Config.ARGB_8888, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError arg0) {
-                    }
-                });
-                VolleyQueueController.getInstance(SplashActivity.this).getRequestQueue().add(irequest);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-        loading_start.startAnimation(scaleAnim);
-
-    }
-
-    private void startActivity() {
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-        startActivity(intent);
-        overridePendingTransition(android.R.anim.fade_in,
-                android.R.anim.fade_out);
+        img_start.setImageResource(R.mipmap.start);
+        goActy(MainActivity.class);
         finish();
     }
 
-    public void saveBitmap(File file, Bitmap bitmap) {
-        if (file.exists()){
-            file.delete();
+    private void initImage(final String imgUrl) {
+        if (ValueUtils.isStrEmpty(imgUrl)){
+            img_start.setImageResource(R.mipmap.start);
         }
-        try {
-            FileOutputStream fos = new FileOutputStream(file);
-            bitmap.compress(Bitmap.CompressFormat.JPEG,100,fos);
-            fos.flush();
-            fos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        mQueue=VolleyQueueController.getInstance(this).getRequestQueue();
+        img_start.setImageUrl(imgUrl, new ImageLoader(mQueue, new BitmapCache()));
+
+        // 图片动画
+        Animation animation = new ScaleAnimation(1.0f, 1.2f, 1.0f, 1.2f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f); // 将图片放大1.2倍，从中心开始缩放
+        animation.setDuration(2000); // 动画持续时间
+        animation.setFillAfter(true); // 动画结束后停留在结束的位置
+        animation.setAnimationListener(SplashActivity.this); // 添加动画监听
+        img_start.startAnimation(animation); // 启动动画
+
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        // 动画结束时跳转至首页
+        goActy(MainActivity.class);
+        finish();
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
 
     }
 }
