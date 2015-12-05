01e65ebf478ebafc70c322f2bdea1e2a662bee7e
Jupiter <619990679@qq.com>
2015-12-05 19:24:59 +0800

    menu修改
    menu修改

--- app/src/main/java/com/tian/zhihu/ui/activity/MainActivity.java
+++ app/src/main/java/com/tian/zhihu/ui/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.tian.zhihu.ui.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
@@ -12,7 +14,10 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.tian.zhihu.R;
 import com.tian.zhihu.base.BaseActivity;
@@ -30,9 +35,16 @@ import com.tian.zhihu.utils.ValueUtils;
 
 import java.util.ArrayList;
 
-public class MainActivity extends BaseActivity implements UIDataListener<ZhihuThemeList> {
+public class MainActivity extends BaseActivity implements UIDataListener<ZhihuThemeList>,View.OnClickListener {
 
-    private ListView main_menu_list;
+
+    private RelativeLayout main_menu;
+    private LinearLayout navi_login_layout;
+    private TextView navi_tv_backup;
+    private TextView navi_tv_download;
+    private TextView navi_tv_main;
+
+    private ListView navi_listview;
     private MenuItemAdapter adapter;
     private ArrayList<ZhihuTheme> mList=new ArrayList<ZhihuTheme>();
 
@@ -48,6 +60,7 @@ public class MainActivity extends BaseActivity implements UIDataListener<ZhihuTh
     protected void installViews() {
         setContentView(R.layout.activity_main);
 
+        // enable ActionBar app icon to behave as action to toggle nav drawer
         setSupportActionBar(base_toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(R.string.app_name);
@@ -61,13 +74,21 @@ public class MainActivity extends BaseActivity implements UIDataListener<ZhihuTh
     }
 
     private void findViews(){
-        main_menu_list= (ListView) this.findViewById(R.id.main_menu_list);
-        drawerLayout= (DrawerLayout) this.findViewById(R.id.drawer_layout);
+        drawerLayout = (DrawerLayout) this.findViewById(R.id.drawer_layout);
+        initMenuView();
         drawerToggle = new ActionBarDrawerToggle(this,
                 drawerLayout,
                 base_toolbar,
                 R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close);
+                R.string.navigation_drawer_close) {
+            public void onDrawerClosed(View view) {
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
         //设置监听drawer切换
         drawerLayout.setDrawerListener(drawerToggle);
         //该方法会自动和actionBar关联, 将开关的图片显示在了action上，如果不设置，也可以有抽屉的效果，不过是默认的图标
@@ -76,15 +97,36 @@ public class MainActivity extends BaseActivity implements UIDataListener<ZhihuTh
         getZhihuTheme();
     }
 
+    private void initMenuView(){
+        main_menu= (RelativeLayout) this.findViewById(R.id.main_menu);
+        View view = LayoutInflater.from(this).inflate(R.layout.frag_navigationdrawer, null);
+        navi_login_layout= (LinearLayout) view.findViewById(R.id.navi_login_layout);
+        navi_tv_backup= (TextView) view.findViewById(R.id.navi_tv_backup);
+        navi_tv_download= (TextView) view.findViewById(R.id.navi_tv_download);
+        navi_tv_main= (TextView) view.findViewById(R.id.navi_tv_main);
+        navi_listview= (ListView) view.findViewById(R.id.navi_listview);
+        navi_login_layout.setOnClickListener(this);
+        navi_tv_backup.setOnClickListener(this);
+        navi_tv_download.setOnClickListener(this);
+        navi_tv_main.setOnClickListener(this);
+
+        main_menu.addView(view);
+    }
+    /** Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
     @Override
     protected void registerEvents() {
-        main_menu_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        navi_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 drawerLayout.closeDrawers();
                 LogUtils.e(TAG, "position==" + position);
-                String themeId=mList.get(position-1).id;
-                String name=mList.get(position-1).name;
+                String themeId=mList.get(position).id;
+                String name=mList.get(position).name;
                 getSupportActionBar().setTitle(""+name);
                 NewsFragment fragment=NewsFragment.newInstance(themeId,name);
                 replaceFragment(fragment, null, R.id.main_content);
@@ -100,10 +142,10 @@ public class MainActivity extends BaseActivity implements UIDataListener<ZhihuTh
 
     private void setMenuList()
     {
-        LayoutInflater inflater = LayoutInflater.from(this);
-        main_menu_list.addHeaderView(inflater.inflate(R.layout.nav_header, main_menu_list, false));
-        adapter=new MenuItemAdapter(this,mList);
-        main_menu_list.setAdapter(adapter);
+        LogUtils.e(TAG,"mList.size()=="+mList.size());
+        adapter=new MenuItemAdapter(MainActivity.this,mList);
+        navi_listview.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 
     @Override
@@ -115,6 +157,9 @@ public class MainActivity extends BaseActivity implements UIDataListener<ZhihuTh
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -127,6 +172,23 @@ public class MainActivity extends BaseActivity implements UIDataListener<ZhihuTh
 
         return super.onOptionsItemSelected(item);
     }
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
 
     @Override
     public void onDataChanged(ZhihuThemeList data) {
@@ -134,6 +196,7 @@ public class MainActivity extends BaseActivity implements UIDataListener<ZhihuTh
             mList=data.others;
             String themeId=mList.get(0).id;
             String name=mList.get(0).name;
+            getSupportActionBar().setTitle(""+name);
             NewsFragment fragment=NewsFragment.newInstance(themeId,name);
             showFragment(fragment,null, R.id.main_content);
             setMenuList();
@@ -143,5 +206,23 @@ public class MainActivity extends BaseActivity implements UIDataListener<ZhihuTh
     @Override
     public void onErrorHappened(String errorCode, String errorMessage) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.navi_login_layout:
+                LogUtils.e(TAG,"navi_login_layout");
+                break;
+            case R.id.navi_tv_backup:
+                LogUtils.e(TAG,"navi_tv_backup");
+                break;
+            case R.id.navi_tv_download:
+                LogUtils.e(TAG,"navi_tv_download");
+                break;
+            case R.id.navi_tv_main:
+                LogUtils.e(TAG,"navi_tv_main");
+                break;
+        }
     }
 }
